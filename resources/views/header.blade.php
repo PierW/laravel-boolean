901c5863660587e5e3a0affe292e22a1d5f77f72
Pier.W <factory666@live.it>
2019-05-17 16:49:52 +0200

    Navbar done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,15 @@
+<nav>
+  <div class="left-side">
+    <img src="https://www.boolean.careers/images/common/logo.png" alt="logo">
+  </div>
+  <div class="right-side">
+    <ul>
+      <li>home</li>
+      <li>corso</li>
+      <li>metodo</li>
+      <li>dopo il corso</li>
+      <li>faq</li>
+    </ul>
+    <button type="button" name="button">Iscriviti</button>
+  </div>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Boolean</title>
+    <!-- MIO CSS -->
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+  </head>
+  <body>
+    @include('header')
+    @yield('content')
+    @include('footer')
+  </body>
+</html>
