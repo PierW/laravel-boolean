edaf1ae64abf169a402f0e9186ad3b568bdf620d
Pier.W <factory666@live.it>
2019-05-21 01:46:56 +0200

    Main done

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,10 +1,33 @@
 <main>
   <div class="main">
     <div class="main-left">
-      Prova
+      <h1>Diventa sviluppatore web</h1>
+      <p>6 mesi di <strong>corso intensivo online in diretta</strong> per imparare
+         da zero il mestiere di web developer e <strong>trovare un nuovo lavoro</strong> nelle aziende top del settore
+      </p>
+      <button type="button" name="button">SCOPRI IL CORSO</button>
     </div>
-    <div class="main-right">
-      Prova
+    <div class="main-right"></div>
+    <div class="back"></div>
+  </div>
+  <div class="partner">
+    <h2>I nostri studenti sono stati assunti da</h2>
+    <div class="logo-partner">
+      <div class="col-auto">
+        <img src="https://www.boolean.careers/images/partners/facile_it.png" alt="facile.it">
+      </div>
+      <div class="col-auto">
+        <img src="https://www.boolean.careers/images/partners/oneday.png" alt="">
+      </div>
+      <div class="col-auto">
+        <img src="https://www.boolean.careers/images/partners/cayenne.png" alt="">
+      </div>
+      <div class="col-auto">
+        <img src="https://www.boolean.careers/images/partners/softec.png" alt="">
+      </div>
+      <div class="col-auto">
+        <img src="https://www.boolean.careers/images/partners/fabbrica_digitale.png" alt="">
+      </div>
     </div>
   </div>
 </main>
